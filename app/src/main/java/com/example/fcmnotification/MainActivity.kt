0b875992b41a5d31b408c5f88ac18a50d1c8f835
rajdeeps <rajdeepsarkar271@gmail.com>
2022-10-28 09:01:02 +0530

    Added Comments

--- app/src/main/java/com/example/fcmnotification/MainActivity.kt
+++ app/src/main/java/com/example/fcmnotification/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.fcmnotification
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+/**
+ * 1. This Project is based on Firebase.
+ * 2. A Notification is pushed from Firebase to Application through FCM.
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
